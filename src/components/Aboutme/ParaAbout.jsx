6423e0b7ca9dbe7e923f2b629dea83009d730118
Louay Bejaoui <47712690+LouayBejaoui@users.noreply.github.com>
2024-01-31 10:01:41 +0100

    final1.1

--- src/components/Aboutme/ParaAbout.jsx
+++ src/components/Aboutme/ParaAbout.jsx
@@ -4,7 +4,7 @@ function ParaAbout(){
   return(
     <div className="paraAbout">
       <h3>Get to know me</h3>
-<p>Hello there! I'm Louay Bejaoui, a 25-year-old programming enthusiast holding a Bachelor's degree in Cybersecurity and a Master's in Computer Engineering. With a keen interest in web development, I've successfully tackled numerous projects as a freelancer and on a personal level, showcasing my dedication and skills in crafting innovative solutions. My portfolio is a testament to my technical proficiency and creative approach to problem-solving. Eager to bring my expertise to a collaborative team, I am confident in my ability to contribute meaningfully and drive success..</p>
+<p>Hello there! I'm Louay Bejaoui, a 25-year-old programming enthusiast holding a Bachelor's degree in Cybersecurity and a Master's in Computer Engineering. With a keen interest in web development, I've successfully tackled numerous projects as a freelancer and on a personal level, showcasing my dedication and skills in crafting innovative solutions. My portfolio is a testament to my technical proficiency and creative approach to problem-solving. Eager to bring my expertise to a collaborative team, I am confident in my ability to contribute meaningfully and drive success.</p>
     </div>
   )
 }
